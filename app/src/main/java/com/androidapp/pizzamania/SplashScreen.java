24a57f357b1298a69675ed7401a9d3f9ee2fc18f
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2025-08-27 17:44:34 +0530

    connected the Databases firebase and SQLite

--- app/src/main/java/com/androidapp/pizzamania/SplashScreen.java
+++ app/src/main/java/com/androidapp/pizzamania/SplashScreen.java
@@ -6,12 +6,20 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class SplashScreen extends AppCompatActivity {
+
+    private FireBaseHelper fireBaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fireBaseHelper = new FireBaseHelper("message");
+        fireBaseHelper.writeDate("Hello");
 
         new Handler().postDelayed(new Runnable() {
             @Override
