e4528c7efa59aba68f6b89dc70982a94d273e738
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2025-09-04 20:18:04 +0530

    feat:search branch on maps activity

--- app/src/main/java/com/androidapp/pizzamania/MapsActivity.java
+++ app/src/main/java/com/androidapp/pizzamania/MapsActivity.java
@@ -2,7 +2,15 @@ package com.androidapp.pizzamania;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,9 +20,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.androidapp.pizzamania.databinding.ActivityMapsBinding;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private EditText txtSearch;
+    private Button btnSearch;
+    private LatLng searchedLatLang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +37,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         setContentView(R.layout.activity_maps);
 
+        txtSearch = findViewById(R.id.search_bar);
+        btnSearch = findViewById(R.id.btnConfirm);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         if(mapFragment != null){
             mapFragment.getMapAsync(this);
         }
+
+        txtSearch.setOnEditorActionListener((textView, actionId, keyEvent) -> {
+            if ((actionId == EditorInfo.IME_ACTION_SEARCH) || (keyEvent != null) && (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+                searchLocation();
+                return true;
+            }
+
+            return false;
+        });
+
+        btnSearch.setOnClickListener(v -> {
+            if (searchedLatLang != null){
+                Intent addBranchIntent = new Intent();
+                addBranchIntent.putExtra("lat", searchedLatLang.latitude);
+                addBranchIntent.putExtra("long", searchedLatLang.longitude);
+                setResult(RESULT_OK, addBranchIntent);
+                finish();
+            }else{
+                Toast.makeText(this, "Please Select a Location", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     /**
      * Manipulates the map once available.
@@ -52,5 +92,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(colombo).title("Marker in colombo"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(colombo, 10));
 
+    }
+
+    private void searchLocation(){
+        String enteredLocation = txtSearch.getText().toString();
+        if (enteredLocation.isEmpty()) {
+            return;
+        }
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+
+            List<Address> addressList = geocoder.getFromLocationName(enteredLocation + ", Sri Lanka", 1);
+
+            if ((addressList != null) && (!addressList.isEmpty())) {
+                Address address = addressList.get(0);
+                searchedLatLang = new LatLng(address.getLatitude(), address.getLongitude());
+
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(searchedLatLang).title(enteredLocation));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(searchedLatLang, 10));
+            }else {
+                Toast.makeText(this, "Invalid Location", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            Toast.makeText(this, "Error Finding Location", Toast.LENGTH_SHORT).show();
+        }
     }
 }
